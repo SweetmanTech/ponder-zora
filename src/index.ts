d35af5d432b434c1e83ca84d576b2199441da8a5
Sweets Sweetman <sweetmantech@gmail.com>
2023-12-23 15:26:26 -0600

    I init Zora 1155 Factory indexing.

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,20 @@ ponder.on("zoraProtocolRewards:RewardsDeposit", async ({ event, context }) => {
     }
   });
 });
+
+ponder.on("zora1155Factory:SetupNewToken", async ({ event, context }) => {
+  const { SetupNewToken } = context.db;
+
+  await SetupNewToken.create({
+    id: `${context.network.name}-${event.block.hash}-${event.log.logIndex}`,
+    data: {
+      contractAddress: event.log.address,
+      chain: context.network.name,
+      tokenId: event.args.tokenId,
+      sender: event.args.sender,
+      maxSupply: event.args.maxSupply,
+      newURI: event.args.newURI,
+      timestamp: event.block.timestamp,
+    }
+  });
+});
