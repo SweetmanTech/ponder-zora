7ed173c628bed3409123a0273e1d8343a13357df
crazzywizard <crazzywizard@gmail.com>
2024-02-04 21:03:21 -0500

    new changes

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,17 +1,22 @@
 import { createConfig, mergeAbis } from "@ponder/core";
-import { createPublicClient, http, parseAbiItem } from "viem";
+import { createPublicClient, http, parseAbiItem, fallback } from "viem";
 import { zoraProtocolRewardsAbi } from "./abis/zoraProtocolRewards";
 import { zoraFactoryImplementationAbi } from "./abis/zoraFactoryImplementation";
 import { zora1155ImplementationAbi } from "./abis/zora1155Implementation";
 
+const trasportOpts = {
+  retryCount: 10,
+  timeout: 100_000
+}
+
 const latestBlockMainnet = await createPublicClient({
-  transport: http(process.env.PONDER_RPC_URL_1),
+  transport: fallback([http(process.env.PONDER_RPC_URL_1_FALLBACK, trasportOpts),http(process.env.PONDER_RPC_URL_1, trasportOpts), http(process.env.PONDER_ETH_INFURA, trasportOpts)],{rank:true}),
 }).getBlock();
 const latestBlockBase = await createPublicClient({
-  transport: http(process.env.PONDER_RPC_URL_8453),
+  transport: fallback([http(process.env.PONDER_RPC_URL_8453_FALLBACK, trasportOpts),http(process.env.PONDER_RPC_URL_8453, trasportOpts),http(process.env.PONDER_BASE_RPC, trasportOpts)],{rank:true}),
 }).getBlock();
 const latestBlockOptimism = await createPublicClient({
-  transport: http(process.env.PONDER_RPC_URL_10),
+  transport: fallback([http(process.env.PONDER_RPC_URL_10_FALLBACK, trasportOpts),http(process.env.PONDER_RPC_URL_10, trasportOpts), http(process.env.PONDER_OPT_INFURA, trasportOpts)],{rank:true}),
 }).getBlock();
 const latestBlockZora = await createPublicClient({
   transport: http(process.env.PONDER_RPC_URL_7777777),
@@ -23,17 +28,17 @@ export default createConfig({
     mainnet: {
       chainId: 1,
       maxHistoricalTaskConcurrency: 8,
-      transport: http(process.env.PONDER_RPC_URL_1),
+      transport: fallback([http(process.env.PONDER_RPC_URL_1_FALLBACK, trasportOpts),http(process.env.PONDER_RPC_URL_1, trasportOpts), http(process.env.PONDER_ETH_INFURA, trasportOpts)],{rank:true}),
     },
     base: {
       maxHistoricalTaskConcurrency: 8,
       chainId: 8453,
-      transport: http(process.env.PONDER_RPC_URL_8453),
+      transport: fallback([http(process.env.PONDER_RPC_URL_8453_FALLBACK, trasportOpts),http(process.env.PONDER_RPC_URL_8453, trasportOpts),http(process.env.PONDER_BASE_RPC, trasportOpts)],{rank:true}),
     },
     optimism: {
       chainId: 10,
       maxHistoricalTaskConcurrency: 8,
-      transport: http(process.env.PONDER_RPC_URL_10),
+      transport: fallback([http(process.env.PONDER_RPC_URL_10_FALLBACK, trasportOpts),http(process.env.PONDER_RPC_URL_10, trasportOpts), http(process.env.PONDER_OPT_INFURA, trasportOpts)],{rank:true}),
     },
     zora: {
       chainId: 7777777,
